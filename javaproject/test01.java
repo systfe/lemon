7e36847bccd5ccfba9c1395661105579104b3d81
public count <140318821+systfe@users.noreply.github.com>
2023-08-22 16:47:55 +0800

    modified:   javaproject/test01.java

--- javaproject/test01.java
+++ javaproject/test01.java
@@ -6,7 +6,7 @@ public class test01 {
     public static void main(String[] args) {
         Collection c=new ArrayList();
         c.add("1");
-        c.add("args"):
+        c.add("args");
         Iterator it =c.iterator();
         
     }
