4e0764955236f67a2128f29ef23cb7b41471f0e7
public count <140318821+systfe@users.noreply.github.com>
2023-08-24 12:19:12 +0800

    modified:   javaproject/test01.java

--- javaproject/test01.java
+++ javaproject/test01.java
@@ -1,14 +1,29 @@
 package javaproject;
-import java.util.ArrayList;
+
 import java.util.Collection;
-import java.util.Iterator;
+import java.util.Comparator;
+import java.util.TreeSet;
 
 public class test01 {
-    public static void main(String[] args) {
-        Collection c=new ArrayList();
+    int n;
+    String s;
+
+    public test01(final int n, final String s) {
+        this.n = n;
+        this.s = s;
+    }
+
+    public static void main(final String[] args) {
+        final Collection<Object> c = new TreeSet<>(
+        new Comparator<Object>() {
+            @Override
+            public int compare(Object arg0, Object arg1) {
+                return 0;
+            }
+        });
         c.add("1");
         c.add("args");
-        Iterator it =c.iterator();
-        
+
     }
+
 }
