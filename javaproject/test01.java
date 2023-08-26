89f91c44def88007af4650ce34c9df81fd47e745
public count <140318821+systfe@users.noreply.github.com>
2023-08-26 14:27:53 +0800

    modified:   javaproject/test01.java

--- javaproject/test01.java
+++ javaproject/test01.java
@@ -1,45 +1,33 @@
 package javaproject;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class test01 {
-    public static void main(String[] args) throws Exception {
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("test01.class"));
-        oos.writeObject(new Student("张三", 20));
-        oos.writeObject(new Student("李四", 21));
-        oos.writeObject(new Student("王五", 22));
-        oos.flush();
-        oos.close();
+    public static void main(String[] args){
+        asd a=new asd();
+        asd s=new asd();
+        a.start();
+        
+        s.start();
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+    }
 
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("test01.class"));
-        
-        System.out.println(ois.readObject());
-        
-        
-        ois.close();
+}
+/**
+ * Innertest01
+ */
+class asd extends Thread {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
     }
     
-}
-class Student implements Serializable{
-    private String name;
-    private int age;
-    public Student(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-    public String toString() {
-        return "姓名：" + name + " 年龄：" + age;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public int getAge() {
-        return age;
-    }
+    
 }
